33bd7702361406208f90be9e4be6333cdf0883fc
Siddharth <siddharth.muscat@gmail.com>
2021-07-08 19:56:57 +0530

    Write tests

--- src/test/index.test.mjs
+++ src/test/index.test.mjs
@@ -1,4 +1,29 @@
 import test from 'ava';
+import {sip, unsip} from '../index.mjs';
 
-test.todo('Compression works');
-test.todo('compression does not increase the size');
+function getRandomString(length) {
+	let result = '';
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	const charactersLength = characters.length;
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() *
+			charactersLength));
+	}
+
+	return result;
+}
+
+test('compression works', t => {
+	const randomString = getRandomString(64);
+	const compressed = sip(randomString);
+	const uncompressed = unsip(compressed);
+
+	t.is(randomString, uncompressed);
+});
+
+test('compression does not increase size', t => {
+	const randomString = getRandomString(64);
+	const compressed = sip(randomString);
+
+	t.true(compressed.length < randomString.length);
+});
