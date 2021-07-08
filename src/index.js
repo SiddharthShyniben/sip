58e6b9200b8055455a483150254017ba87aa17fc
Siddharth <siddharth.muscat@gmail.com>
2021-07-08 15:17:34 +0530

    Implement sip and unsip functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-export function sip() {
-	
+import {compress, decompress} from './compress';
+
+export function sip(data) {
+	return compress(data);
 }
 
 export function unsip() {
-	
+	return decompress(data);
 }
 
 export function sipPath() {
