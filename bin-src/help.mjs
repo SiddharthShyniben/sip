b6db5ca362a9b73d2f97a37456f80b5d1875f401
Siddharth <siddharth.muscat@gmail.com>
2021-07-09 11:58:43 +0530

    Add function to log help

---
+++ bin-src/help.mjs
@@ -0,0 +1,25 @@
+export function printHelp(endMessage = '') {
+	console.log(
+`
+szip 1.0
+usage: sip [-cdhioqVv] [-s .suffix] [<file> [<file> ...]
+
+-c --stdout        write output stdout and keep files
+   --to-stdout
+-d --decompress    uncompress files
+   --uncompress    
+-h --help          show this help
+-o --output        specify path to output to
+-q --quiet         be quiet
+-V --version       show version information
+-v --verbose       print extra statistics
+
+-s .suf            use the .suf suffix instead of .sip
+   --suffix .suf
+-i text            compress text and write it to the stdout 
+   --input text
+
+${endMessage}
+`
+	)
+}
