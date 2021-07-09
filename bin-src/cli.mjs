41e4c8759f201f579fe57bc91e6556d21ac62678
Siddharth <siddharth.muscat@gmail.com>
2021-07-09 11:59:58 +0530

    Implement --text option

--- bin-src/cli.mjs
+++ bin-src/cli.mjs
@@ -1,1 +1,13 @@
-console.error('Not implemented!');
+import minimist from 'minimist';
+import {printHelp} from './help.mjs';
+
+// Imports break this
+const {sip} = require('../dist/index.js');
+
+const argv = minimist(process.argv.slice(2));
+
+if (argv.i || argv.input) {
+	console.log(sip(argv.i || argv.input));
+} else {
+	printHelp();
+}
