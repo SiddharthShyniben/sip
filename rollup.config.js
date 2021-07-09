9fbdff43d416e4b1ebc1584eea8287cdec5cf727
Siddharth <siddharth.muscat@gmail.com>
2021-07-09 09:42:14 +0530

    Update rollup config with second build step

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,20 @@
-const config = {
-	input: 'src/index.mjs',
-	output: {
-		dir: 'dist',
-		format: 'umd',
-		name: 'sip'
+const config = [
+	{
+		input: 'src/index.mjs',
+		output: {
+			dir: 'dist',
+			format: 'umd',
+			name: 'sip'
+		}
+	},
+	{
+		input: 'bin-src/cli.mjs',
+		output: {
+			banner: '#!/usr/bin/env node',		
+			dir: 'bin',
+			format: 'cjs'
+		}
 	}
-};
+];
 
 export default config;
