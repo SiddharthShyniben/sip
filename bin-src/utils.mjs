bde30fc0437ee2ad1b87973def471da0a9b73699
Siddharth <siddharth.muscat@gmail.com>
2021-07-09 14:14:47 +0530

    Add util to get byte count from string

--- bin-src/utils.mjs
+++ bin-src/utils.mjs
@@ -9,3 +9,7 @@ export function formatBytes(bytes, decimals = 2) {
 
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
+
+export function getByteCount(string) {
+	return Buffer.byteLength(string, 'utf8')
+}
